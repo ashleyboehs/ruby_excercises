54582deffe86dac17e6badec3c26e5678b921ffc
Ashley Boehs <ashley@boehs.com>
2023-10-20 15:18:32 -0500

    Solution 3

---
+++ Solution3.rb
@@ -0,0 +1,17 @@
+#3. Write a Ruby program to create a new string which is n copies of a given string where n in a non-negative integer.
+
+def multiple_string(str, n)
+  return str*n 
+end
+
+print mutliple_string('a', 1), "\n"
+print mutliple_string('a', 2), "\n"
+print mutliple_string('a', 3), "\n"
+print mutliple_string('a', 4), "\n"
+print mutliple_string('a', 5), "\n"
+
+#4. Write a Ruby program which accept the radius of a circle from the user and compute the parameter and area.
+
+
+
+
